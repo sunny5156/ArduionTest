988344b1d060ce3fe33c940762457bd14f8a7ebf
Adam Thompson <adam@serialphotog.com>
2016-07-21 10:48:48 +0800

    温度开关

---
+++ arduionTempSwitch/arduionTempSwitch.ino
@@ -0,0 +1,62 @@
+  /*
+   功能：通过协议读取DHT11数据，并在串口显示温湿度
+   */
+      //   
+      //   FILE:  dht_test.pde  
+      // PURPOSE: DHT library test sketch for Arduino  
+      //  
+        
+  #include <dht.h>  
+    
+  dht DHT;  
+    
+  #define DHT11_PIN 4//put the sensor in the digital pin 4  
+  int SWITCH_PIN = 3;
+    
+  void setup()  
+  {  
+    pinMode(SWITCH_PIN, OUTPUT);
+    Serial.begin(9600);  
+    Serial.println("DHT TEST PROGRAM ");  
+    Serial.print("LIBRARY VERSION: ");  
+    Serial.println(DHT_LIB_VERSION);  
+    Serial.println();  
+    Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");  
+  }  
+    
+  void loop()  
+  {  
+    
+    // READ DATA  
+   Serial.print("DHT11, \t");  
+   int chk = DHT.read11(DHT11_PIN);  
+    switch (chk)  
+    {  
+      case 0:  Serial.print("OK,\t"); break;  
+      case -1: Serial.print("Checksum error,\t"); break;  
+      case -2: Serial.print("Time out error,\t"); break;  
+      default: Serial.print("Unknown error,\t"); break;  
+    }  
+   // DISPLAT DATA  
+    Serial.print(DHT.humidity,1);  
+    Serial.print(",\t");  
+    Serial.println(DHT.temperature,1);  
+    
+    if(DHT.temperature >= 27.0){
+      Serial.print("open");
+       Serial.println(digitalRead(SWITCH_PIN));
+       digitalWrite(SWITCH_PIN, LOW); 
+    }else{
+      Serial.print("close");
+    
+     digitalWrite(SWITCH_PIN, HIGH); 
+    }
+    
+    delay(1000);  
+  }  
+  //  
+  // END OF FILE  
+  //  
+      
+  
+
