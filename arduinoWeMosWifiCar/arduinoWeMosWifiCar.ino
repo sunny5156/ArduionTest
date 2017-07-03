62adfc7e1a024461f224e173b24103aa5ac43d19
sunny5156 <137898350@qq.com>
2017-07-03 10:39:17 +0800

    edit

--- arduinoWeMosWifiCar/arduinoWeMosWifiCar.ino
+++ arduinoWeMosWifiCar/arduinoWeMosWifiCar.ino
@@ -29,6 +29,7 @@ void setup() {
   //使用ESP自带的LED，HIGH灯灭，可换其它管脚
   //pinMode(LED_BUILTIN, OUTPUT);
   //digitalWrite(LED_BUILTIN, HIGH);
+  //PWM_PLUS();
 }
 
 void loop() {
@@ -62,13 +63,65 @@ void loop() {
   //给客户端的反馈信息
   String s = "HTTP/1.1 200 OK\r\n";
   s += "Content-Type:text/html; charset=UTF-8\r\n\r\n";
-  s += "<!DOCTYPEHTML>\r\n<html>\r\n";
-  s += "<a href='/U'>前进</a><br/><br/>";
-  s += "<a href='/D'>后退</a><br/><br/>";
-  s += "<a href='/L'>左转</a><br/><br/>";
-  s += "<a href='/R'>右转</a><br/><br/>";
-  s += "<a href='/S'>停车</a><br/><br/>";
-  s += String(val) + "</html>\n";
+  //s += "<!DOCTYPEHTML>\r\n<html>\r\n";
+  s += "<!DOCTYPE html>";
+  s += "<html lang='zh-CN'>";
+  s += "<head>";
+  s += "  <meta charset='utf-8'>";
+  s += "  <title>上下左右</title>";
+  s += "  <style>";
+  s += "      * {";
+  s += "          padding: 0;";
+  s += "          margin: 0;";
+  s += "      }";
+  s += "      .content,";
+  s += "      .content ul {";
+  s += "          width: 100%;";
+  s += "          height: auto;";
+  s += "          overflow: hidden;";
+  s += "     }";
+  s += "      .content li {";
+  s += "          list-style: none;";
+  s += "          float: left;";
+  s += "          width: 50%;";
+  s += "          text-align: center;";
+  s += "          margin: 1em 0;";
+  s += "      }";
+  s += "      .content li:nth-child(1),";
+  s += "      .content li:nth-child(4),";
+  s += "      .content li:nth-child(5),";
+  s += "      .content li:nth-child(6) {";
+  s += "          width: 100%;";
+  s += "      }";
+  s += "      .content li span {";
+  s += "          padding: 1em;";
+  s += "          border: 1px solid #dedede;";
+  s += "          text-align: center;";
+  s += "          line-height: 4em;";
+  s += "          box-sizing: border-box;";
+  s += "      }";
+  s += "  </style>";
+  s += "</head>";
+  s += "<body>";
+  s += "  <div class='content'>";
+  s += "      <ul>";
+  s += "          <li><a href='/L'><span class='left'>左</span></a></li>";
+  s += "          <li><a href='/D'><span class='down'>下</span></a></li>";
+  s += "          <li><a href='/U'><span class='up'>上</span></a></li>";
+  s += "          <li><a href='/R'><span class='right'>右</span></a></li>";
+  s += "          <li><a href='/S'><span class='a'>A</span></a></li>";
+  s += "          <li><span class='b'>B</span></li>";
+  s += "      </ul>";
+  s += "  </div>";
+  s += String(val);
+  s += "</body>";
+  s += "</html>";
+  //s += "<a href='/U'>前进</a><br/><br/>";
+  //s += "<a href='/D'>后退</a><br/><br/>";
+  //s += "<a href='/L'>左转</a><br/><br/>";
+  //s += "<a href='/R'>右转</a><br/><br/>";
+  //s += "<a href='/S'>停车</a><br/><br/>";
+  //s += String(val) + "</html>\n";
   client.flush();
   client.print(s);
   delay(1);
@@ -80,30 +133,30 @@ void Forword()
   Serial.println("前进");
   digitalWrite(N1, LOW);
   digitalWrite(N2, HIGH);
-  digitalWrite(N3, LOW);
-  digitalWrite(N4, HIGH);
+  digitalWrite(N3, HIGH);
+  digitalWrite(N4, LOW);
 }
 void Backword()
 {
   Serial.println("后退");
   digitalWrite(N1, HIGH);
   digitalWrite(N2, LOW);
-  digitalWrite(N3, HIGH);
-  digitalWrite(N4, LOW);
+  digitalWrite(N3, LOW);
+  digitalWrite(N4, HIGH);
 }
 void TurnLeft()
 {
   Serial.println("左转");
-  digitalWrite(N1, HIGH);
-  digitalWrite(N2, LOW);
+  digitalWrite(N1, LOW);
+  digitalWrite(N2, HIGH);
   digitalWrite(N3, LOW);
   digitalWrite(N4, HIGH);
 }
 void TurnRight()
 {
   Serial.println("右转");
-  digitalWrite(N1, LOW);
-  digitalWrite(N2, HIGH);
+  digitalWrite(N1, HIGH);
+  digitalWrite(N2, LOW);
   digitalWrite(N3, HIGH);
   digitalWrite(N4, LOW);
 }
